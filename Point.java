0cac5445c1710302ab7949c0e52e882c75930755
s-samchenko <ssamchenko05@gmail.com>
2024-10-27 15:14:19 -0400

    Fixed bugs in CircleTest

--- Point.java
+++ Point.java
@@ -1,14 +1,14 @@
-package lab4;
+package Lab5;
 
 public class Point {
 	double x;
 	double y;
-	
+
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public double distance(Point other) {
 		return Math.sqrt(Math.pow(x-other.x, 2) + Math.pow(y-other.y, 2));
 	}
--- Triangle.java
+++ Triangle.java
@@ -1,20 +1,20 @@
-package lab4;
+package Lab5;
 
 public class Triangle {
 	Point A;
 	Point B;
 	Point C;
-	
+
 	public Triangle(Point A, Point B, Point C) {
 		this.A = A;
 		this.B = B;
 		this.C = C;
 	}
-	
+
 	public double perimeter() {
 		return A.distance(B) + B.distance(C) + C.distance(A);
 	}
-	
+
 	public boolean isEquilateral() {
 		double dAB = A.distance(B);
 		double dBC = B.distance(C);
