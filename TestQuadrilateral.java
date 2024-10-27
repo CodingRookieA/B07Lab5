eecdffa8c92fb8621a85b5f5f33731f25ee03b78
EthanCen1 <125598322+EthanCen1@users.noreply.github.com>
2024-10-27 13:05:09 -0400

    Update TestQuadrilateral.java

--- TestQuadrilateral.java
+++ TestQuadrilateral.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
-public class QuadrilateralTest {
+public class TestQuadrilateral {
 
     // Test the perimeter method with larger values
     @Test
